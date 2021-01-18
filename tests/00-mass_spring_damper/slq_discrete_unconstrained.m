ded020db54d0669005cf92a3de03f855927a910c
Gonzalo Paz Delgado <gonzalopd96@uma.es>
2021-01-18 14:16:48 +0100

    Update the simple mass model with the latest improvements in the SLQ algorithm

--- tests/00-mass_spring_damper/slq_discrete_unconstrained.m
+++ tests/00-mass_spring_damper/slq_discrete_unconstrained.m
@@ -1,8 +1,10 @@
-init = cputime;
+tic
+lineSearchStep = 0.2;
+
 % System properties
 m = 1;
-k = 1;
-b = 0.2;
+k = 10;
+b = 2;
 
 % Constraints
 ti = 0;
@@ -11,14 +13,11 @@ vi = 0;
 
 tf = 15;
 yf = 1;
-vf = 100;
+vf = 0;
 
-Ff = [1 0;
-     0 1]; 
-hf = [yf;
-     vf];
+g = 9.81;
  
-dt = 0.01;
+dt = 0.05;
 t = 0:dt:tf;
 
 % State vector
@@ -27,14 +26,14 @@ x(1,1) = yi;
 x(2,1) = vi;
 
 % Initial control law
-u = zeros(1,size(t,2));
+u = zeros(1,size(t,2)-1);
 
 % Target state and control trajectories
 x0 = zeros(2,size(t,2));
 x0(1,end) = yf;
 x0(2,end) = vf;
 
-u0 = zeros(1,size(t,2));
+u0 = zeros(1,size(t,2)-1);
 
 % SLQR algorithm
 iter = 1;
@@ -42,27 +41,35 @@ while 1
     % Forward integrate system equations
     for i = 2:size(t,2)
         a = (u(i-1) - k*x(1,i-1) - b*x(2,i-1))/m;
+        x(1,i) = x(1,i-1) + dt*x(2,i-1);
         x(2,i) = x(2,i-1) + dt*a;
-        x(1,i) = x(1,i-1) + dt*x(2,i);
     end
     
     xh0 = x0 - x;
     uh0 = u0 - u;
     
     % Quadratize cost function along the trajectory
-    Q = [1 0;
-         0 1];
-    Qend = [1000000 0;
-            0  1000000];
+    Q = zeros(size(x,1),size(x,1),size(t,2));
+    for i = 1:size(t,2)
+        Q(:,:,i) = zeros(size(x,1));
+    end
+    
+    Qend = [10000000000000 0;
+            0  10000000000000];
     R = 1;
 
-    % Linearize the system dynamics and constraints along the trajectory    
-    A = [1        dt;
-         -dt*k/m  1-dt*b/m]; 
-    B = [0;
-         dt/m]; 
-    C = [1 0]; 
-    D = 0;
+    % Linearize the system dynamics and constraints along the trajectory  
+    A = zeros(size(x,1),size(x,1),size(t,2));
+    for i = 1:size(t,2)
+        A(:,:,i) = [1        dt;
+                    -dt*k/m  1-dt*b/m]; 
+    end
+     
+    B = zeros(size(x,1),size(u,1),size(t,2));
+    for i = 1:size(t,2)
+        B(:,:,i) = [0;
+                    dt/m]; 
+    end
     
     % LQ problem solution
     M = zeros(size(B,1),size(B,1),size(t,2));
@@ -71,47 +78,49 @@ while 1
     P(:,:,end) = Qend;
     s(:,:,end) = -Qend*xh0(:,end);
     
-    xh = zeros(2,size(t,2));
-    uh = zeros(1,size(t,2));
+    xh = zeros(size(x,1),size(t,2));
+    uh = zeros(size(u,1),size(t,2)-1);
     v = zeros(size(xh));
     lambdah = zeros(size(s));    
 %     xh(1,1) = yi;
 %     xh(2,1) = vi;
-    
+
     % Solve backward
     for i = size(t,2)-1:-1:1
-        M(:,:,i) = inv(eye(size(B,1)) + B*inv(R)*B.'*P(:,:,i+1));
-        P(:,:,i) = Q + A.'*P(:,:,i+1)*M(:,:,i)*A;
-        s(:,:,i) = A.'*(eye(size(Q,1)) - P(:,:,i+1)*M(:,:,i)*B*inv(R)*B.')*s(:,:,i+1)+...
-            A.'*P(:,:,i+1)*M(:,:,i)*B*uh0(i) - Q*xh0(:,i);
+        M(:,:,i) = inv(eye(size(B,1)) + B(:,:,i)/R*B(:,:,i).'*P(:,:,i+1));
+        P(:,:,i) = Q(:,:,i) + A(:,:,i).'*P(:,:,i+1)*M(:,:,i)*A(:,:,i);
+        s(:,:,i) = A(:,:,i).'*(eye(size(Q,1)) - P(:,:,i+1)*M(:,:,i)*B(:,:,i)/R*B(:,:,i).')*s(:,:,i+1)+...
+            A(:,:,i).'*P(:,:,i+1)*M(:,:,i)*B(:,:,i)*uh0(:,i) - Q(:,:,i)*xh0(:,i);
     end
     
     % Solve forward
     for i = 1:size(t,2)-1
-        v(:,i) = M(:,:,i)*B*(uh0(i)-R*B.'*s(:,:,i+1));
-        xh(:,i+1) = M(:,:,i)*A*xh(:,i)+v(:,i);
+        v(:,i) = M(:,:,i)*B(:,:,i)*(uh0(:,i)-R\B(:,:,i).'*s(:,:,i+1));
+        xh(:,i+1) = M(:,:,i)*A(:,:,i)*xh(:,i)+v(:,i);
         lambdah(:,:,i+1) = P(:,:,i+1)*xh(:,i+1)+s(:,:,i+1);
-        uh(i) = uh0(i)-inv(R)*B.'*lambdah(:,:,i+1);
+        uh(:,i) = uh0(:,i)-R\B(:,:,i).'*lambdah(:,:,i+1);
     end
     
+    
     % Exit condition
-    if norm(uh)<0.01*norm(u)
+    if norm(uh)<0.0001*norm(u)
         break;
     else
         % Line search to optimize alfa
-        alfa = 1:-0.01:0.0001;
+        alfa = 1:-lineSearchStep:0.0001;
         J = zeros(size(alfa));
         uk = u;
         for n = 1:size(alfa,2)
             u = uk + alfa(n)*uh;
             for i = 2:size(t,2)
                 a = (u(i-1) - k*x(1,i-1) - b*x(2,i-1))/m;
+                x(1,i) = x(1,i-1) + dt*x(2,i-1);
                 x(2,i) = x(2,i-1) + dt*a;
-                x(1,i) = x(1,i-1) + dt*x(2,i);
             end
             J(n) = 1/2*(x(:,end)-x0(:,end)).'*Qend*(x(:,end)-x0(:,end));
             for i = 1:size(t,2)-1
-                J(n) = J(n) + 1/2*((x(:,i)-x0(:,i)).'*Q*(x(:,i)-x0(:,i)) + (u(:,i)-u0(:,i)).'*R*(u(:,i)-u0(:,i)));
+                J(n) = J(n) + 1/2*((x(:,i)-x0(:,i)).'*Q(:,:,i)*(x(:,i)-x0(:,i))...
+                    + (u(:,i)-u0(:,i)).'*R*(u(:,i)-u0(:,i)));
             end            
         end
         [mincost, ind] = min(J);
@@ -119,15 +128,14 @@ while 1
         
         % Update controller
         u = uk + alfamin*uh;
+        iter = iter+1;
     end   
 
-    iter = iter+1;
 end
-endt = cputime;
-disp(['SLQ found the optimal control input within ',num2str(iter-1),' iterations'])
-disp(['Elapsed execution time: ',num2str(endt-init),' seconds'])
-iu = cumsum(u);
-disp(['Total force applied: ',num2str(iu(end)),' N*s'])
+disp(['SLQ found the optimal control input within ',num2str(iter),' iterations'])
+toc
+iu = cumsum(abs(u));
+disp(['Total force applied: ',num2str(iu(end)),' N'])
 
 figure(1)
 plot(t,x(1,:))
@@ -146,7 +154,7 @@ hold on
 plot(tf,vf,'Marker','o','MarkerFaceColor','red')
 
 figure(3)
-plot(t,u)
+plot(t(1:end-1),u)
 title('Actuating force (u)','interpreter','latex')
 xlabel('t(s)','interpreter','latex')
 ylabel('F(N)','interpreter','latex')
