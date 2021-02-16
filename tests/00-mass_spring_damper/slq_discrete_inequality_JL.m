c11c7e990bfd48e33acd738db69b85416715798e
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2021-02-16 09:56:41 +0100

    Create new scripts for solving the Jacobson&Lele optimization problem
    - New script solves the unconstrained J&L problem with the old SLQ algorithm.
    - Another new script solves the constrained J&L problem with the new SLQ algorithm. Still needed some fixes for pure state constraints.

---
+++ tests/00-mass_spring_damper/slq_discrete_inequality_JL.m
@@ -0,0 +1,505 @@
+clear
+tic
+
+% Constraints
+ti = 0;
+tf = 1;
+
+uMax = 5;
+uMin = -5;
+
+dt = 0.01;
+t = 0:dt:tf;
+
+% State vector
+numStates = 2;
+x = zeros(numStates,size(t,2));
+x(1,1) = 0;
+x(2,:) = -1;
+
+% Initial control law
+numInputs = 1;
+u = zeros(numInputs,size(t,2));
+
+% Target state and control trajectories
+x0 = zeros(numStates,size(t,2));
+x0(1,end) = 0;
+x0(2,end) = 0;
+
+u0 = zeros(numInputs,size(t,2));
+
+% Constraints matrices definition
+% State input constraints
+numStateInputConstraints = 0;
+I0 = zeros(numStateInputConstraints,size(t,2));
+I = I0;
+C = zeros(numStateInputConstraints,numStates,size(t,2));
+D = zeros(numStateInputConstraints,numInputs,size(t,2));
+r = zeros(numStateInputConstraints,size(t,2));
+
+% D(1,1,:) = 1;
+% r(1,:) = -uMax;
+% 
+% D(2,1,:) = -1;
+% r(2,:) = uMin;
+
+% Pure state constraints
+numPureStateConstraints = 1;
+J0 = zeros(numPureStateConstraints,size(t,2));
+J = J0;
+G = zeros(numPureStateConstraints,numStates,size(t,2));
+h = zeros(numPureStateConstraints,size(t,2));
+
+G(1,2,:) = 1;
+for k = 1:size(t,2)
+    h(1,k) = -8*((k+1)*dt-0.5)^2+0.5;
+end
+
+% SLQR algorithm
+iter = 0;
+while 1   
+    % Forward integrate system dynamics
+    for i = 2:size(t,2)
+        x(1,i) = x(1,i-1) + dt*x(2,i-1);
+        x(2,i) = x(2,i-1)*(1-dt) + dt*u(1,i);
+    end
+    
+    figure(1)
+    subplot(2,1,1)
+    plot(t,x(1,:),'Color','r')
+    hold on
+    plot(t,x(2,:),'b')
+    plot(t,-h,'g')
+    legend('$x_1$','$x_2$','State constraint','interpreter','latex')
+    title('State constrained','interpreter','latex')
+    xlabel('Time','interpreter','latex')
+    ylabel('Magnitude','interpreter','latex')
+    axis([0 1 -1 1])
+    hold off
+    subplot(2,1,2)
+    plot(t,u)
+    title('Control trajectory','interpreter','latex')
+    xlabel('Time','interpreter','latex')
+    ylabel('Magnitude','interpreter','latex')
+    hold off
+    
+    xs = zeros(numStates,size(t,2));
+    us = zeros(numInputs,size(t,2));
+        
+    % Quadratize cost function along the trajectory
+    Q = zeros(numStates,numStates,size(t,2));
+    for i = 1:size(t,2)
+        Q(:,:,i) = [0.0200 0.0001;
+                    0.0001 0.0198];
+    end
+    
+    Q(:,:,end) = [0.0200 0.0001;
+                  0.0001 0.0198];
+        
+    R = zeros(numInputs,numInputs,size(t,2));
+    for i = 1:size(t,2)
+        R(:,:,i) = 1.0066e-4*eye(numInputs,numInputs);
+    end
+    
+    K = zeros(numStates,numInputs,size(t,2));
+    for i = 1:size(t,2)
+        K(:,:,i) = 1e-4*[0.0033;
+                         0.9901];
+    end
+    
+    % Define the sequential state and input vectors
+    xs0 = zeros(numStates,size(t,2));
+    us0 = zeros(numInputs,size(t,2));
+
+    for i = 1:size(t,2)
+%         xs0(:,i) = Q(:,:,i)*(x(:,i) - x0(:,i));
+%         us0(:,i) = R(:,:,i)*(u(:,i) - u0(:,i));
+        
+        % Según Sideris, debería ser como sigue, aunque con esto no estamos
+        % realimentando el vector de referencia
+        xs0(:,i) = Q(:,:,i)*x(:,i) + K(:,:,i)*u(:,i) + x0(:,i);
+        us0(:,i) = R(:,:,i)*u(:,i) + K(:,:,i).'*x(:,i) + u0(:,i);
+    end
+    xs0(:,end) = Q(:,:,end)*x(:,end) + x0(:,end);
+
+    
+    % Linearize the system dynamics and constraints along the trajectory  
+    A = zeros(numStates,numStates,size(t,2));
+    for i = 1:size(t,2)
+        A(:,:,i) = [1 0.01
+                    0 0.99]; 
+    end
+     
+    B = zeros(numStates,numInputs,size(t,2));
+    for i = 1:size(t,2)
+        B(:,:,i) = [0.00;
+                    0.01]; 
+    end
+    
+    % Active Constraints definition
+    % State input constraints
+    tl = [];
+    p = cell(size(t,2),1);
+    paux = zeros(numStateInputConstraints,1);
+    pl = zeros(numStateInputConstraints,1);
+    
+    for i = 1:size(t,2)
+        anyActiveConstraint = false;
+        for j = 1:numStateInputConstraints
+            if I(j,i)
+                p{i} = [p{i}; j];
+                paux(j) = 1;
+                anyActiveConstraint = true;
+            end
+        end
+        if anyActiveConstraint
+            tl = [tl i];
+        end
+    end
+
+    numActiveSIConstraints = sum(paux);
+    lp = zeros(numActiveSIConstraints,1);
+
+    index = 1;
+    for i = 1:numStateInputConstraints
+        if paux(i)
+            pl(i) = index;
+            lp(index) = i;
+            index = index+1;
+        end
+    end
+    
+    Cl = zeros(numActiveSIConstraints,numStates,size(t,2));
+    Dl = zeros(numActiveSIConstraints,numInputs,size(t,2));
+    rl = zeros(numActiveSIConstraints,size(t,2));
+    
+    for i = tl
+        Cl(pl(p{i}),:,i) = C(p{i},:,i);
+        Dl(pl(p{i}),:,i) = D(p{i},:,i);
+%         rl(pl(p{i}),i) = r(p{i},i);
+    end
+    
+    
+    % Pure state constraints
+    tk = [];
+    q = cell(size(t,2),1);
+    qaux = zeros(numPureStateConstraints,1);
+    ql = zeros(numPureStateConstraints,1);
+
+    for i = 1:size(t,2)
+        anyActiveConstraint = false;
+        for j = 1:numPureStateConstraints
+            if J(j,i)
+                q{i} = [q{i}; j];
+                qaux(j) = 1;
+                anyActiveConstraint = true;
+            end
+        end
+        if anyActiveConstraint
+            tk = [tk i];
+        end
+    end
+
+    numActivePSConstraints = sum(qaux);
+    lq = zeros(numActivePSConstraints,1);
+    
+    index = 1;
+    for i = 1:numPureStateConstraints
+        if qaux(i)
+            ql(i) = index;
+            lq(index) = i;
+            index = index+1;
+        end
+    end
+    
+    
+    Gk = zeros(numActivePSConstraints,numStates,size(t,2));
+    hk = zeros(numActivePSConstraints,size(t,2));
+    
+    for i = tk
+        Gk(ql(q{i}),:,i) = G(q{i},:,i);
+%         hk(ql(q{i}),i) = h(q{i},i);
+    end
+    
+    % Predefinitions
+    Dh = zeros(numActiveSIConstraints,numActiveSIConstraints,size(t,2));
+    E = zeros(numActiveSIConstraints,numStates,size(t,2));
+    rh = zeros(numActiveSIConstraints,size(t,2));
+    
+    Ah = zeros(numStates,numStates,size(t,2));
+    Rh = zeros(numStates,numStates,size(t,2));
+    Qh = zeros(numStates,numStates,size(t,2));
+    x0h = zeros(numStates,size(t,2));
+    u0h = zeros(numStates,size(t,2));
+    
+    for i = tl
+        Dh(pl(p{i}),pl(p{i}),i) = inv(Dl(pl(p{i}),:,i)/R(:,:,i)*Dl(pl(p{i}),:,i).');
+    end
+    
+    for i = 1:size(t,2)
+
+        E(:,:,i) = Cl(:,:,i) - Dl(:,:,i)/R(:,:,i)*K(:,:,i).';
+        rh(:,i) = rl(:,i) - Dl(:,:,i)/R(:,:,i)*us0(:,i);
+        
+        Ah(:,:,i) = A(:,:,i) - B(:,:,i)/R(:,:,i)*(K(:,:,i).' + Dl(:,:,i).'*Dh(:,:,i)*E(:,:,i));
+        Rh(:,:,i) = B(:,:,i)/R(:,:,i)*(eye(numInputs,numInputs)-Dl(:,:,i).'*Dh(:,:,i)*Dl(:,:,i)/R(:,:,i))*B(:,:,i).';
+        Qh(:,:,i) = Q(:,:,i) - K(:,:,i)/R(:,:,i)*K(:,:,i).' + E(:,:,i).'*Dh(:,:,i)*E(:,:,i);
+        x0h(:,i) = xs0(:,i) - K(:,:,i)/R(:,:,i)*us0(:,i).' + E(:,:,i).'*Dh(:,:,i)*rh(:,i);
+        u0h(:,i) = -B(:,:,i)/R(:,:,i)*(us0(:,i) + Dl(:,:,i).'*Dh(:,:,i)*rh(:,i));
+    end
+    
+    % LQ problem solution
+    M = zeros(numStates,numStates,size(t,2));
+    P = zeros(numStates,numStates,size(t,2));
+    z = zeros(numStates,size(t,2));
+    P(:,:,end) = Q(:,:,end);
+    z(:,end) = xs0(:,end);
+    
+    % Solve backward
+    for i = size(t,2)-1:-1:1
+        M(:,:,i) = inv(eye(numStates) + Rh(:,:,i)*P(:,:,i+1));
+        P(:,:,i) = Qh(:,:,i) + Ah(:,:,i).'*P(:,:,i+1)*M(:,:,i)*Ah(:,:,i);
+        z(:,i) = Ah(:,:,i).'*M(:,:,i).'*z(:,i+1) + Ah(:,:,i).'*P(:,:,i+1)*M(:,:,i)*u0h(:,i) + x0h(:,i);
+    end
+    
+    Gamma = zeros(numActivePSConstraints*size(tk,2),numStates);
+    Gammak = zeros(numActivePSConstraints,numStates,size(t,2), size(tk,2));
+    y = zeros(numActivePSConstraints*size(tk,2),1);
+    F = zeros(numActivePSConstraints*size(tk,2),numActivePSConstraints*size(tk,2));
+    H = zeros(numActivePSConstraints*size(tk,2),1);
+
+    % Solve over all state constraints   
+    for k = 1:size(tk,2)
+        yk = zeros(numActivePSConstraints,tk(k));
+
+        Gammak(:,:,tk(k),k) = Gk(:,:,tk(k));
+
+        for n = tk(k)-1:-1:1
+            Gammak(:,:,n,k) = Gammak(:,:,n+1,k)*M(:,:,n)*Ah(:,:,n);
+            yk(:,n) = yk(:,n+1) + Gammak(:,:,n+1,k)*M(:,:,n)*(u0h(:,n) - Rh(:,:,n) * z(:,n+1));
+        end
+
+        H((k-1)*numActivePSConstraints+1:k*numActivePSConstraints) = hk(:,tk(k));
+        Gamma((k-1)*numActivePSConstraints+1:k*numActivePSConstraints,:) = Gammak(:,:,1,k);
+        y((k-1)*numActivePSConstraints+1:k*numActivePSConstraints) = yk(:,1);        
+
+    end
+    for k = 1:size(tk,2)
+        for j = 1:size(tk,2)
+            minConstraintIndex = min(tk(k),tk(j));
+            Fkj = zeros(numActivePSConstraints,numActivePSConstraints,minConstraintIndex);
+            for n = minConstraintIndex-1:-1:1
+                Fkj(:,:,n) = Fkj(:,:,n+1) - Gammak(:,:,n+1,k)*M(:,:,n)*Rh(:,:,n)*Gammak(:,:,n+1,j).';
+            end
+            F((k-1)*numActivePSConstraints+1:k*numActivePSConstraints,...
+              (j-1)*numActivePSConstraints+1:j*numActivePSConstraints) = Fkj(:,:,1);
+        end
+    end
+    
+    nuV = zeros(numActivePSConstraints*size(tk,2),1);
+    
+%     invF = zeros(size(F));
+%     if(~det(F))
+%         Faux = F;
+%         currentIndex = 0;
+%         correctIndex = [];
+%         for i = tk
+%             for j = 1:size(q{i},1)
+%                 correctIndex = [correctIndex currentIndex+ql(q{i}(j))];
+%             end
+%             currentIndex = currentIndex + numActivePSConstraints;
+%         end
+%         
+%         Faux = F(correctIndex,correctIndex);
+%         invFaux = inv(Faux);
+%         invF(correctIndex,correctIndex) = invFaux;
+%     else
+%         invF = inv(F);
+%     end    
+%     nuV(:) = invF*(-Gamma*xs(:,1) - y - H);
+
+    nuV(:) = F\(-Gamma*xs(:,1) - y - H);
+    
+    nu = zeros(numActivePSConstraints,size(t,2));
+    for i = 1:size(tk,2)
+        nu(:,tk(i)) = nuV((i-1)*numActivePSConstraints+1:i*numActivePSConstraints);
+    end   
+    
+    s = zeros(numStates,size(t,2));
+    if numActivePSConstraints
+        for i = 1:size(t,2)
+            sumG = 0;
+            for k = 1:size(tk,2)
+                if (tk(k) >= i)
+                    sumG = sumG + Gammak(:,:,i,k).'*nu(:,tk(k));
+                end
+            end
+            s(:,i) = z(:,i) + sumG;
+        end
+    else
+        s(:,:) = z(:,:);
+    end
+    
+    v = zeros(numStates,size(t,2));
+    lambda = zeros(numStates,size(t,2));
+    mu = zeros(numActiveSIConstraints,size(t,2));
+
+%     xsk(:,1) = zeros(numStates,1);
+%     xs(:,1) = xs0(:,1);
+
+    % Solve forward
+    for i = 1:size(t,2)-1
+        v(:,i) = M(:,:,i)*(u0h(:,i) - Rh(:,:,i)*s(:,i+1));
+        xs(:,i+1) = M(:,:,i)*Ah(:,:,i)*xs(:,i) + v(:,i);
+        lambda(:,i+1) = P(:,:,i+1)*xs(:,i+1) + s(:,i+1);
+        mu(:,i) = Dh(:,:,i)*(E(:,:,i)*xs(:,i) - Dl(:,:,i)/R(:,:,i)*B(:,:,i).'*lambda(:,i+1) + rh(:,i));
+        us(:,i) = -R(:,:,i)\(K(:,:,i).'*xs(:,i) + B(:,:,i).'*lambda(:,i+1) + Dl(:,:,i).'*mu(:,i) + us0(:,i));
+    end
+    iter = iter+1;
+
+    step3 = true;
+    if norm(us)>=0.0001*norm(u)
+        % Step 2
+        rhoi = ones(numStateInputConstraints,size(t,2));
+        deltai = ones(numStateInputConstraints,size(t,2));
+        
+        rhoj = ones(numPureStateConstraints,size(t,2));
+        deltaj = ones(numPureStateConstraints,size(t,2));
+
+        for n = 1:size(t,2)
+            for i = 1:numStateInputConstraints
+                if(~I(i,n))
+                    rhoi(i,n) = C(i,:,n)*x(:,n) + D(i,:,n)*u(:,n) + r(i,n);
+                    deltai(i,n) = C(i,:,n)*xs(:,n) + D(i,:,n)*us(:,n);
+                end
+            end
+            for j = 1:numPureStateConstraints
+                if(~J(j,n))
+                    rhoj(j,n) = G(j,:,n)*x(:,n) + h(j,n);
+                    deltaj(j,n) = G(j,:,n)*xs(:,n);
+                end
+            end
+        end
+        
+        thetak = min(-rhoi(~I & deltai>0)./deltai(~I & deltai>0));
+        betak = min(-rhoj(~J & deltaj>0)./deltaj(~J & deltaj>0));
+
+        alfak = min([1 thetak betak]);
+        
+        % Update controller
+%         x = x + alfak*xs;
+        u = u + alfak*us;
+        
+        if alfak == 1
+            step3 = true;
+        else
+            step3 = false;
+            for n = 1:size(t,2)
+                for i = 1:numStateInputConstraints
+                    if(-rhoi(i,n)/deltai(i,n) == alfak && ~I(i,n) && n < size(t,2))
+                        I(i,n) = 1;
+                    end
+                end
+                for j = 1:numPureStateConstraints
+                    if(-rhoj(j,n)/deltaj(j,n) == alfak && ~J(j,n)&& n > 1)
+                        J(j,n) = 1;
+                    end
+                end
+            end
+        end
+    end
+    if step3        
+        % Step 3
+        if size(tl,2) > 0
+            minMu = zeros(1,size(t,2));
+            iS = zeros(1,size(t,2));
+
+            for m = tl
+                minMu(m) = 99999999;
+                for i = 1:size(p{m},1)
+                    if I(p{m}(i),m) && ~I0(p{m}(i),m) && mu(i,m) < minMu(m)
+                        minMu(m) = mu(i,m);
+                        iS(m) = i;
+                    end
+                end
+                if minMu(m) == 99999999
+                    minMu(m) = 0;
+                end
+            end
+            
+            [minimumMu, maux] = min(minMu(1,tl));
+            mS = tl(maux);
+        else
+            minimumMu = 0;
+            mS = 1;
+            iS = 1;
+        end
+
+        if size(tk,2) > 0
+            minNu = zeros(1,size(t,2));
+            jS = zeros(1,size(t,2));
+
+            for l = tk
+                minNu(1,l) = 99999999;
+                for j = 1:size(q{l},1)
+                    if J(q{l}(j),l) && ~J0(q{l}(j),l) && nu(j,l) < minNu(1,l)
+                        minNu(1,l) = nu(j,l);
+                        jS(l) = j;
+                    end
+                end
+                if minNu(1,l) == 99999999
+                    minNu(1,l) = 0;
+                end
+            end
+            [minimumNu, laux] = min(minNu(1,tk));
+            lS = tk(laux);
+
+        else
+            minimumNu = 0;
+            lS = 1;
+            jS = 1;
+        end
+        
+        
+        if minimumMu >= -1e-5 && minimumNu >=-1e-5 && norm(us)<0.0001*norm(u)
+            for i = 2:size(t,2)
+                x(1,i) = x(1,i-1) + dt*x(2,i-1);
+                x(2,i) = x(2,i-1)*(1-dt) + dt*u(1,i);
+            end
+            break;
+        else
+            if minimumMu >= minimumNu && size(p{mS},1) > 0
+                I(p{mS}(iS(mS)),mS) = 0;
+            elseif minimumMu < minimumNu && size(q{lS},1) > 0
+                J(q{lS}(jS(lS)),lS) = 0;
+            end
+        end
+    end
+    
+end
+disp(['SLQ found the optimal control input within ',num2str(iter),' iterations'])
+toc
+
+
+% h = zeros(1,size(t,2));
+% for k = 1:size(t,2)
+%     h(1,k) = -8*((k+1)*dt-0.5)^2+0.5;
+% end
+figure(1)
+subplot(2,1,1)
+plot(t,x(1,:),'Color','r')
+hold on
+plot(t,x(2,:),'b')
+plot(t,-h,'g')
+legend('$x_1$','$x_2$','State constraint','interpreter','latex')
+title('State constrained','interpreter','latex')
+xlabel('Time','interpreter','latex')
+ylabel('Magnitude','interpreter','latex')
+axis([0 1 -1 1])
+hold off
+subplot(2,1,2)
+plot(t,u)
+title('Control trajectory','interpreter','latex')
+xlabel('Time','interpreter','latex')
+ylabel('Magnitude','interpreter','latex')
+hold off
---
+++ tests/00-mass_spring_damper/slq_discrete_unconstrained_JL.m
@@ -0,0 +1,150 @@
+tic
+lineSearchStep = 0.2;
+
+% Constraints
+ti = 0;
+tf = 1;
+
+dt = 0.01;
+t = 0:dt:tf;
+
+% State vector
+x = zeros(2,size(t,2));
+x(1,1) = 0;
+x(2,1) = -1;
+
+% Initial control law
+u = zeros(1,size(t,2));
+
+% Target state and control trajectories
+x0 = zeros(2,size(t,2));
+u0 = zeros(1,size(t,2));
+
+% SLQR algorithm
+iter = 1;
+while 1   
+    % Forward integrate system equations
+    for i = 2:size(t,2)
+        x(1,i) = x(1,i-1) + dt*x(2,i-1);
+        x(2,i) = x(2,i-1)*(1-dt) + dt*u(1,i);
+    end
+    
+    xh0 = x0 - x;
+    uh0 = u0 - u;
+    
+    % Quadratize cost function along the trajectory
+    Q = zeros(size(x,1),size(x,1),size(t,2));
+    for i = 1:size(t,2)
+        Q(:,:,i) = [0.0200 0.0001;
+                    0.0001 0.0198];
+    end
+
+    R = 1.0066e-4;
+
+    % Linearize the system dynamics and constraints along the trajectory  
+    A = zeros(size(x,1),size(x,1),size(t,2));
+    for i = 1:size(t,2)
+        A(:,:,i) = [1 0.01
+                    0 0.99]; 
+    end
+     
+    B = zeros(size(x,1),size(u,1),size(t,2));
+    for i = 1:size(t,2)
+        B(:,:,i) = [0.00;
+                    0.01]; 
+    end
+    
+    % LQ problem solution
+    M = zeros(size(B,1),size(B,1),size(t,2));
+    P = zeros(size(Q,1),size(Q,2),size(t,2));
+    s = zeros(size(Q,1),1,size(t,2));
+    P(:,:,end) = Q(:,:,end);
+    s(:,:,end) = -Q(:,:,end)*xh0(:,end);
+    
+    xh = zeros(size(x,1),size(t,2));
+    uh = zeros(size(u,1),size(t,2));
+    v = zeros(size(xh));
+    lambdah = zeros(size(s));    
+%     xh(1,1) = yi;
+%     xh(2,1) = vi;
+
+    % Solve backward
+    for i = size(t,2)-1:-1:1
+        M(:,:,i) = inv(eye(size(B,1)) + B(:,:,i)/R*B(:,:,i).'*P(:,:,i+1));
+        P(:,:,i) = Q(:,:,i) + A(:,:,i).'*P(:,:,i+1)*M(:,:,i)*A(:,:,i);
+        s(:,:,i) = A(:,:,i).'*(eye(size(Q,1)) - P(:,:,i+1)*M(:,:,i)*B(:,:,i)/R*B(:,:,i).')*s(:,:,i+1)+...
+            A(:,:,i).'*P(:,:,i+1)*M(:,:,i)*B(:,:,i)*uh0(:,i) - Q(:,:,i)*xh0(:,i);
+    end
+    
+    % Solve forward
+    for i = 1:size(t,2)-1
+        v(:,i) = M(:,:,i)*B(:,:,i)*(uh0(:,i)-R\B(:,:,i).'*s(:,:,i+1));
+        xh(:,i+1) = M(:,:,i)*A(:,:,i)*xh(:,i)+v(:,i);
+        lambdah(:,:,i+1) = P(:,:,i+1)*xh(:,i+1)+s(:,:,i+1);
+        uh(:,i) = uh0(:,i)-R\B(:,:,i).'*lambdah(:,:,i+1);
+    end
+    
+    
+    % Exit condition
+    if norm(uh)<0.0001*norm(u)
+        break;
+    else
+        % Line search to optimize alfa
+        alfa = 1:-lineSearchStep:0.0001;
+        J = zeros(size(alfa));
+        uk = u;
+        for n = 1:size(alfa,2)
+            u = uk + alfa(n)*uh;
+            for i = 2:size(t,2)
+                x(1,i) = x(1,i-1) + dt*x(2,i-1);
+                x(2,i) = x(2,i-1)*(1-dt) + dt*u(1,i);
+            end
+            J(n) = 1/2*(x(:,end)-x0(:,end)).'*Q(:,:,end)*(x(:,end)-x0(:,end));
+            for i = 1:size(t,2)-1
+                J(n) = J(n) + 1/2*((x(:,i)-x0(:,i)).'*Q(:,:,i)*(x(:,i)-x0(:,i))...
+                    + (u(:,i)-u0(:,i)).'*R*(u(:,i)-u0(:,i)));
+            end            
+        end
+        [mincost, ind] = min(J);
+        alfamin = alfa(ind);
+        
+        % Update controller
+        u = uk + alfamin*uh;
+        iter = iter+1;
+    end   
+
+end
+disp(['SLQ found the optimal control input within ',num2str(iter),' iterations'])
+toc
+
+for i = 2:size(t,2)
+    x(1,i) = x(1,i-1) + dt*x(2,i-1);
+    x(2,i) = x(2,i-1)*(1-dt) + dt*u(1,i);
+end
+
+h = zeros(1,size(t,2));
+for k = 1:size(t,2)
+    h(1,k) = -8*((k+1)*dt-0.5)^2+0.5;
+end
+
+figure
+subplot(2,1,1)
+plot(t,x(1,:),'Color','r')
+hold on
+plot(t,x(2,:),'b')
+plot(t,-h,'g')
+legend('$x_1$','$x_2$','State constraint','interpreter','latex')
+title('State constrained','interpreter','latex')
+xlabel('Time','interpreter','latex')
+ylabel('Magnitude','interpreter','latex')
+axis([0 1 -1 1])
+hold off
+subplot(2,1,2)
+plot(t,u)
+title('Control trajectory','interpreter','latex')
+xlabel('Time','interpreter','latex')
+ylabel('Magnitude','interpreter','latex')
+hold off
+
+
+
