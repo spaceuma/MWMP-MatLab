e306756a3833b78de3656bd4004c942a5268cfb8
Gonzalo Paz Delgado <gonzalopd96@uma.es>
2021-05-14 11:38:35 +0200

    Include small comments on several functions

--- src/utils/modulateYaw.m
+++ src/utils/modulateYaw.m
@@ -1,4 +1,5 @@
 function modulatedYaw = modulateYaw(yaw,reference)
+%MODULATEYAW Ensure the given yaw is continuous, avoiding jumps
     diff = reference - yaw;
     modulatedYaw = yaw;
     while diff > pi
