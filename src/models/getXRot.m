5045995f891aefc1aac0ac59d132d345866968a6
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2021-09-29 17:16:56 +0200

    Add function to get roll, pitch and yaw from a transformation matrix

--- src/models/getXRot.m
+++ src/models/getXRot.m
@@ -1,4 +1,6 @@
 function Rx = getXRot(angle)
+%getXRot Generates a X rotation matrix Rx(4x4) given a certain turn angle
+
     Rx = [1 0          0           0;
           0 cos(angle) -sin(angle) 0;
           0 sin(angle)  cos(angle) 0;          
@@ -6,8 +8,10 @@ function Rx = getXRot(angle)
       
     for i = 1:4  
         for j = 1:4
-            if(abs(Rx(i,j)) < 1e-6) 
-                Rx(i,j) = 0;
+            if isnumeric(Rx(i,j))
+                if(abs(Rx(i,j)) < 1e-6) 
+                    Rx(i,j) = 0;
+                end
             end
         end
     end
--- src/models/getYRot.m
+++ src/models/getYRot.m
@@ -1,4 +1,6 @@
 function Ry = getYRot(angle)
+%getYRot Generates a Y rotation matrix Ry(4x4) given a certain turn angle
+
     Ry = [ cos(angle) 0 sin(angle) 0;
            0          1 0          0
           -sin(angle) 0 cos(angle) 0;          
@@ -6,8 +8,10 @@ function Ry = getYRot(angle)
        
     for i = 1:4  
         for j = 1:4
-            if(abs(Ry(i,j)) < 1e-6) 
-                Ry(i,j) = 0;
+            if isnumeric(Ry(i,j))
+                if(abs(Ry(i,j)) < 1e-6) 
+                    Ry(i,j) = 0;
+                end
             end
         end
     end
