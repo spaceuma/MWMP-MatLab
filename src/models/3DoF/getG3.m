9a06d7a5d3f2cf44bccbd6cb74e790cbe71febd8
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2022-05-06 12:33:08 +0200

    Simplify gravity matrix computation

--- src/models/3DoF/getG3.m
+++ src/models/3DoF/getG3.m
@@ -1,23 +1,11 @@
-function [G, dG] = getG3(theta1, theta2, theta3, g)
+function [G, dG] = getG3(theta1, theta2, theta3)
+    global m1 m2 m3;
+    global a2 d4;
+
     G = zeros(3,1);
-    dG = zeros(3,3);
     
     G(1,1) = 0;
-    G(2,1) = - 30.681*cos(theta2 + theta3) - 97.34*cos(theta2);
-    G(3,1) = -30.681*cos(theta2 + theta3);
-
-    G = G*g/9.81;
-    
-    dG(1,1) = 0;
-    dG(1,2) = 0;
-    dG(1,3) = 0;
-    dG(2,1) = 0;
-    dG(2,2) = 30.681*sin(theta2 + theta3) + 97.34*sin(theta2);
-    dG(2,3) = 30.681*sin(theta2 + theta3);
-    dG(3,1) = 0;
-    dG(3,2) = 30.681*sin(theta2 + theta3);
-    dG(3,3) = 30.681*sin(theta2 + theta3);
-
-    dG = dG*g/9.81;
+    G(2,1) = -(a2*m2*cos(theta2) + 2*a2*m3*cos(theta2) + d4*m3*cos(theta2 + theta3))/2;
+    G(3,1) = -(d4*m3*cos(theta2 + theta3))/2;
 end
 
