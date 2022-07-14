6476165fdf0e397a5026125442a2f9310544a4a6
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2022-07-14 13:37:29 +0200

    Remove not updated tests

--- tests/05-sherpa_tt_simpleBase (not updated)/getCost.m
+++
@@ -1,9 +0,0 @@
-function cost = getCost(x, y)
-    global costMap;
-    mapResolution = 0.05;
-        
-    ix = round(x/mapResolution)+1;
-    iy = round(y/mapResolution)+1;
-    
-    cost = costMap(ix,iy);
-end
